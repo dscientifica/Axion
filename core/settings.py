fc7d5fca057d2a51597c13cdb6377bb1464c7cdb
Seu Nome <seuemail@gmail.com>
2026-02-18 02:41:43 -0300

    Fix syntax error in settings

--- core/settings.py
+++ core/settings.py
@@ -90,8 +90,9 @@ WSGI_APPLICATION = "core.wsgi.application"
 DATABASES = {
     "default": dj_database_url.config(
         default=os.getenv("DATABASE_URL")
-    }
+    )
 }
+
 
 # ========================
 # PASSWORD VALIDATION
